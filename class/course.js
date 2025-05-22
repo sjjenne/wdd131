a6ab8d09a656c227683724e43c15fbc95e1c5b2f
sjjenne <samanthajanejenne@gmail.com>
2025-05-22 07:26:07 -0600

    blogpt1

---
+++ class/course.js
@@ -0,0 +1,144 @@
+const students = [
+{ last: 'Aultz',
+first: 'Aaron'
+},
+{ last: 'Banda',
+first: 'Tamanda'
+},
+{ last: 'Bangerter',
+first: 'Ryland'
+},
+{ last: 'Boss',
+first: 'Seth'
+},
+{ last: 'Castro',
+first: 'Daniel'
+},
+{ last: 'Christensen',
+first: 'Connor'
+},
+{ last: 'Cockrum',
+first: 'Mason'
+},
+{ last: 'Crisp',
+first: 'Patrick'
+},
+{ last: 'Earl',
+first: 'John'
+},
+{ last: 'Earl',
+first: 'Joseph'
+},
+{ last: 'Earl',
+first: 'Micah'
+},
+{ last: 'Giard',
+first: 'Will'
+},
+{ last: 'Hansen',
+first: 'Justin'
+},
+{ last: 'Harris',
+first: 'Cole'
+},
+{ last: 'Harris',
+first: 'William'
+},
+{ last: 'Henrie',
+first: 'Jacob'
+},
+{ last: 'Jenne',
+first: 'Samantha'
+},
+{ last: 'Lamer',
+first: 'Kian'
+},
+{ last: 'Lee',
+first: 'Ryan'
+},
+{ last: 'Madson',
+first: 'Isaac'
+},
+{ last: 'Merrell',
+first: 'Noah'
+},
+{ last: 'Miller',
+first: 'Raquel'
+},
+{ last: 'Moncada',
+first: 'Joshua'
+},
+{ last: 'Montoya Ugarte',
+first: 'Alan'
+},
+{ last: 'Nielson',
+first: 'Jesse'
+},
+{ last: 'Palmer',
+first: 'Benjamin'
+},
+{ last: 'Platt',
+first: 'Jaeden'
+},
+{ last: 'Quizon',
+first: 'MJ'
+},
+{ last: 'Schmitz',
+first: 'Emma'
+},
+{ last: 'Sheffer',
+first: 'Darren'
+},
+{ last: 'Skousen',
+first: 'Brian'
+},
+{ last: 'Smith',
+first: 'Emory'
+},
+{ last: 'Kasapo',
+first: 'Tyra'
+},
+{ last: 'Taylor',
+first: 'Austin'
+},
+{ last: 'Tunquist',
+first: 'Thomas'
+},
+{ last: 'Ukpong',
+first: 'Robin'
+},
+{ last: 'Volmer',
+first: 'Zachery'
+},
+{ last: 'Wicks',
+first: 'Andrew'
+},
+{ last: 'Williams',
+first: 'Eton'
+},
+{ last: 'Yasa',
+first: 'Manny'
+}
+];
+
+let container = document.querySelector('#student_container');
+
+students.forEach(function(item){
+    //console.log(name.first);
+    let name = document.createElement('section');
+    name.setAttribute('class', 'format');
+
+    let html = `
+        <p>${item.first}</p>
+        <p>${item.last}</p>
+        <hr>
+    ` 
+    // `` above tab key   
+    // ${} to reference variable
+
+    name.innerHTML = html;
+
+    container.appendChild(name);
+
+
+}) // we defined function as we called, hence the parenthesis at the end of the braces.
