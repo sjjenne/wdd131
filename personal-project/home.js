f6e8f7fa7c0da36afc827f50b6dc175d4fae5058
sjjenne <samanthajanejenne@gmail.com>
2025-07-07 20:58:52 -0600

    beginnings-nationalpark

---
+++ personal-project/home.js
@@ -0,0 +1,17 @@
+// let slideIndex = 1
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//     showSlides(slideIndex = n)
+// }
+
+// function showSlides(n) {
+//     let i;
+//     let slides = document.querySelector("#slide");
+//     let dots = document.querySelector(".dot");
+
+// }
