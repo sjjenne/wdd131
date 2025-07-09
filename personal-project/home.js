f5e996f003a18750248c3b0706b5be2ebd9b3bb1
sjjenne <samanthajanejenne@gmail.com>
2025-07-09 15:30:26 -0600

    parks-updates

--- personal-project/home.js
+++ personal-project/home.js
@@ -1,17 +1,26 @@
-// let slideIndex = 1
-// showSlides(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
+function plusSlide(n) {
+    showSlides(slideIndex += n);
+}
 
-// function currentSlide(n) {
-//     showSlides(slideIndex = n)
-// }
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-// function showSlides(n) {
-//     let i;
-//     let slides = document.querySelector("#slide");
-//     let dots = document.querySelector(".dot");
-
-// }
+function showSlides(n) {
+    let i;
+    let slides = document.querySelector("#slide");
+    let dots = document.querySelector(".dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
